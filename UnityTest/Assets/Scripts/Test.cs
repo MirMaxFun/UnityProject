69e08d09fb394ce7ccb656a5b87fd61dbd38f560
MirMaxFun <wayday228@gmail.com>
2023-03-10 16:11:29 +0400

    Update Test.cs

---
+++ UnityTest/Assets/Scripts/Test.cs
@@ -0,0 +1,9 @@
+
+/// <summary>
+/// Called on objects which have been network instantiated with Network.Instantiate.
+/// </summary>
+/// <param name="info">The NetworkMessageInfo for the event.</param>
+void OnNetworkInstantiate(NetworkMessageInfo info)
+{
+    
+}
